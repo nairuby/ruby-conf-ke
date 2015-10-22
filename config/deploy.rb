5f6757e5a034884c9c0e53be47bfbac0f75696dc
Bernard Banta <banta.bernard@gmail.com>
2015-10-22 12:01:02 +0300

    Added capistrano restart task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,13 @@ set :repo_url, 'git@github.com:nairuby/ruby-conf-ke.git'
 set :log_level, :debug
 set :rvm_ruby_version, 'ruby-2.2.1@rails4'
 set :default_stage, 'production'
+
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+end
