1d2d28241a8d83ca0167665a62bde6ef82fed05b
AdamKabaka <michaelkimathi2020@gmail.com>
2016-04-29 11:15:21 +0300

    Updated capistrano configs

--- Capfile
+++ Capfile
@@ -4,6 +4,7 @@ require 'capistrano/setup'
 # Include default deployment tasks
 require 'capistrano/deploy'
 require 'capistrano/rvm'
+require 'capistrano/bundler'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, '/var/www/rubyconf.nairuby.org'
 set :branch, 'master'
 
-role :app, %w{ubuntu@128.199.166.194}
-role :web, %w{ubuntu@128.199.166.194}
-role :db,  %w{ubuntu@128.199.166.194}
+role :app, %w{ubuntu@188.166.218.248}
+role :web, %w{ubuntu@188.166.218.248}
+role :db,  %w{ubuntu@188.166.218.248}
