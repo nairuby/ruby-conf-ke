f6dac3cae89a9a03a090f7ef7021462a50374762
banta <banta.bernard@gmail.com>
2017-01-31 12:04:22 +0300

    Removed capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+# lock '3.4.0'
 
 set :application, 'rubyconf.nairuby.org'
 set :repo_url, 'git@github.com:nairuby/ruby-conf-ke.git'
