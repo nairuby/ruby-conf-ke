26341a9d32053f442d1b6ba080d8865fbd7f2c8d
Danielotieno <danielotieno.ke@gmail.com>
2017-08-22 16:33:26 +0300

    added style files

--- app.rb
+++ app.rb
@@ -69,11 +69,11 @@ end
 
 # 2016
 
-get '/2017' do
+get '/2018' do
   @title = :home
   @speaker = %w( keithpitty.jpg laurenvoswinkel.jpg philiparndt.jpg konstantingredeskoul.png johndalton.jpg shevauncoker.jpg seanmarcia.jpg josspaling.jpg erikmichaelsober.jpg johnbarton.jpg amywibowo.jpg
     scottfeinberg.jpg sabrinaleandro.jpg ).sample
-  haml :"2017/home", :layout => :"2017/layout"
+  haml :"2018/home", :layout => :"2018/layout"
 end
 
 get '/2016' do
@@ -99,7 +99,7 @@ end
 
 get '/' do
   status_code 302
-  redirect '/2017'
+  redirect '/2018'
 end
 
 post '/subscribe' do
